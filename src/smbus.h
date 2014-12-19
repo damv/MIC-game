5163aa8a20b32f15ab1d61d3cd0f28b82d47106d
Damien Vielpeau <damien.vielpeau@gmail.com>
2014-12-19 14:56:28 +0100

    same

--- src/smbus.h
+++ src/smbus.h
@@ -2,18 +2,12 @@
 #define __SMBUS_H__
 
 // INIT
-// Set config
 void SMBUS_init();
 
 // BEGIN
-// Initiate connection
-void SMBUS_begin(unsigned char address);
-
-// WRITE
-// Send via bus the specified value to the device corresponding to the specified address
-void SMBUS_write(unsigned char address, unsigned char value);
+void ACCE_begin();
 
 // READ
-void SMBUS_read(unsigned char address, unsigned char *readByte, bit isLastRead);
+void ACCE_read();
 
 #endif
