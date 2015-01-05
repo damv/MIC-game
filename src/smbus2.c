a9eda8208e610e5b91dacc9cea34669e0240efc2
Damien Vielpeau <damien.vielpeau@gmail.com>
2015-01-05 15:11:29 +0100

    refractoring to match previous interface

--- src/smbus2.c
+++ src/smbus2.c
@@ -54,6 +54,9 @@
 //-----------------------------------------------------------------------------
 
 #include <C8051F310.h>
+#include "utils.h"
+#include "smbus2.h"
+
 
 //-----------------------------------------------------------------------------
 // Global CONSTANTS
@@ -79,7 +82,7 @@
 
 #define  NUM_BYTES_WR   1              // Number of bytes to write
                                        // Master -> Slave
-#define  NUM_BYTES_RD   1              // Number of bytes to read
+#define  NUM_BYTES_RD   6              // Number of bytes to read
                                        // Master <- Slave
 
 //-----------------------------------------------------------------------------
@@ -110,8 +113,6 @@ unsigned long NUM_ERRORS;              // Counter for the number of errors.
 sfr16    TMR3RL   = 0x92;              // Timer3 reload registers
 sfr16    TMR3     = 0x94;              // Timer3 counter registers
 
-sbit LED = P3^3;                       // LED on port P3.3
-
 sbit SDA = P0^0;                       // SMBus on P0.0
 sbit SCL = P0^1;                       // and P0.1
 
@@ -120,114 +121,10 @@ sbit SCL = P0^1;                       // and P0.1
 //-----------------------------------------------------------------------------
 
 void SMBus_Init (void);
-void Timer1_Init (void);
-void Timer3_Init (void);
-void Port_Init (void);
 
 void SMBus_ISR (void);
 void Timer3_ISR (void);
 
-void SMB_Write (void);
-void SMB_Read (void);
-void T0_Wait_ms (unsigned char ms);
-
-//-----------------------------------------------------------------------------
-// MAIN Routine
-//-----------------------------------------------------------------------------
-//
-// Main routine performs all configuration tasks, then loops forever sending
-// and receiving SMBus data to the slave.
-//
-void main (void)
-{
-   volatile unsigned char dat;         // Test counter
-   volatile unsigned char data_count;  // SMB_DATA_IN and SMB_DATA_OUT counter
-   unsigned char i;                    // Dummy variable counters
-
-   PCA0MD &= ~0x40;                    // WDTE = 0 (watchdog timer enable bit)
-
-   OSCICN |= 0x03;                     // Set internal oscillator to highest
-                                       // setting of 24500000
-
-   // If slave is holding SDA low because of an improper SMBus reset or error
-   while(!SDA)
-   {
-      // Provide clock pulses to allow the slave to advance out
-      // of its current state. This will allow it to release SDA.
-      XBR1 = 0x40;                     // Enable Crossbar
-      SCL = 0;                         // Drive the clock low
-      for(i = 0; i < 255; i++);        // Hold the clock low
-      SCL = 1;                         // Release the clock
-      while(!SCL);                     // Wait for open-drain
-                                       // clock output to rise
-      for(i = 0; i < 10; i++);         // Hold the clock high
-      XBR1 = 0x00;                     // Disable Crossbar
-   }
-
-   Port_Init ();                       // Initialize Crossbar and GPIO
-
-   Timer1_Init ();                     // Configure Timer1 for use as SMBus
-                                       // clock source
-
-   Timer3_Init();                      // Configure Timer3 for use with SMBus
-                                       // low timeout detect
-
-   SMBus_Init ();                      // Configure and enable SMBus
-
-   EIE1 |= 0x01;                       // Enable the SMBus interrupt
-
-   LED = 0;
-
-   EA = 1;                             // Global interrupt enable
-
-// TEST CODE-------------------------------------------------------------------
-
-   dat = 0;                            // Output data counter
-   NUM_ERRORS = 0;                     // Error counter
-   while (1)
-   {
-      // SMBus Write Sequence
-      for (data_count = 0; data_count < NUM_BYTES_WR; data_count++)
-      {
-         SMB_DATA_OUT[data_count] = dat; // Define next outgoing byte
-         dat++;
-      }
-      TARGET = SLAVE_ADDR;             // Target the Slave for next SMBus
-                                       // transfer
-      SMB_Write();                     // Initiate SMBus write
-
-      // SMBus Read Sequence
-      TARGET = SLAVE_ADDR;             // Target the Slave for next SMBus
-                                       // transfer
-      SMB_Read();
-
-      // Check transfer data
-      for (data_count = 0; data_count < NUM_BYTES_RD; data_count++)
-      {
-         // Received data match transmit data?
-         if(SMB_DATA_IN[data_count] != SMB_DATA_OUT[data_count])
-         {
-            NUM_ERRORS++;              // Increment error counter if no
-                                       // match
-         }
-      }
-
-      // Indicate that an error has occurred (LED no longer lit)
-      if (NUM_ERRORS > 0) {
-         LED = 0;
-      }
-      else {
-         LED = ~LED;
-      }
-
-      // Run to here to view the SMB_DATA_IN and SMB_DATA_OUT variable arrays
-
-      T0_Wait_ms (1);
-   }
-
-// END TEST CODE---------------------------------------------------------------
-
-}
 
 //-----------------------------------------------------------------------------
 // Initialization Routines
@@ -259,107 +156,6 @@ void SMBus_Init (void)
                                        // Enable SCL low timeout detect;
 
    SMB0CF |= 0x80;                     // Enable SMBus;
-}
-
-//-----------------------------------------------------------------------------
-// Timer1_Init
-//-----------------------------------------------------------------------------
-//
-// Return Value : None
-// Parameters   : None
-//
-// Timer1 configured as the SMBus clock source as follows:
-// - Timer1 in 8-bit auto-reload mode
-// - SYSCLK or SYSCLK / 4 as Timer1 clock source
-// - Timer1 overflow rate => 3 * SMB_FREQUENCY
-// - The resulting SCL clock rate will be ~1/3 the Timer1 overflow rate
-// - Timer1 enabled
-//
-void Timer1_Init (void)
-{
-
-// Make sure the Timer can produce the appropriate frequency in 8-bit mode
-// Supported SMBus Frequencies range from 10kHz to 100kHz.  The CKCON register
-// settings may need to change for frequencies outside this range.
-#if ((SYSCLK/SMB_FREQUENCY/3) < 255)
-   #define SCALE 1
-      CKCON |= 0x08;                   // Timer1 clock source = SYSCLK
-#elif ((SYSCLK/SMB_FREQUENCY/4/3) < 255)
-   #define SCALE 4
-      CKCON |= 0x01;
-      CKCON &= ~0x0A;                  // Timer1 clock source = SYSCLK / 4
-#endif
-
-   TMOD = 0x20;                        // Timer1 in 8-bit auto-reload mode
-
-   // Timer1 configured to overflow at 1/3 the rate defined by SMB_FREQUENCY
-   TH1 = -(SYSCLK/SMB_FREQUENCY/SCALE/3);
-
-   TL1 = TH1;                          // Init Timer1
-
-   TR1 = 1;                            // Timer1 enabled
-}
-
-//-----------------------------------------------------------------------------
-// Timer3_Init
-//-----------------------------------------------------------------------------
-//
-// Return Value : None
-// Parameters   : None
-//
-// Timer3 configured for use by the SMBus low timeout detect feature as
-// follows:
-// - Timer3 in 16-bit auto-reload mode
-// - SYSCLK/12 as Timer3 clock source
-// - Timer3 reload registers loaded for a 25ms overflow period
-// - Timer3 pre-loaded to overflow after 25ms
-// - Timer3 enabled
-//
-void Timer3_Init (void)
-{
-   TMR3CN = 0x00;                      // Timer3 configured for 16-bit auto-
-                                       // reload, low-byte interrupt disabled
-
-   CKCON &= ~0x40;                     // Timer3 uses SYSCLK/12
-
-   TMR3RL = -(SYSCLK/12/40);           // Timer3 configured to overflow after
-   TMR3 = TMR3RL;                      // ~25ms (for SMBus low timeout detect):
-                                       // 1/.025 = 40
-
-   EIE1 |= 0x80;                       // Timer3 interrupt enable
-   TMR3CN |= 0x04;                     // Start Timer3
-}
-
-//-----------------------------------------------------------------------------
-// PORT_Init
-//-----------------------------------------------------------------------------
-//
-// Return Value : None
-// Parameters   : None
-//
-// Configure the Crossbar and GPIO ports.
-//
-// P0.0   digital   open-drain    SMBus SDA
-// P0.1   digital   open-drain    SMBus SCL
-//
-// P3.3   digital   push-pull     LED
-//
-// all other port pins unused
-//
-// Note: If the SMBus is moved, the SCL and SDA sbit declarations must also
-// be adjusted.
-//
-void PORT_Init (void)
-{
-   P0MDOUT = 0x00;                     // All P0 pins open-drain output
-
-   P3MDOUT |= 0x08;                    // Make the LED (P3.3) a push-pull
-                                       // output
-
-   XBR0 = 0x04;                        // Enable SMBus pins
-   XBR1 = 0x40;                        // Enable crossbar and weak pull-ups
-
-   P0 = 0xFF;
 }
 
 //-----------------------------------------------------------------------------
@@ -553,80 +349,107 @@ void SMB_Read (void)
    while (SMB_BUSY);                   // Wait for transfer to complete
 }
 
+
+
 //-----------------------------------------------------------------------------
-// T0_Wait_ms
+//  ACCELERO methods
 //-----------------------------------------------------------------------------
-//
-// Return Value : None
-// Parameters   :
-//   1) unsigned char ms - number of milliseconds to wait
-//                        range is full range of character: 0 to 255
-//
-// Configure Timer0 to wait for <ms> milliseconds using SYSCLK as its time
-// base.
-//
-void T0_Wait_ms (unsigned char ms)
-{
-   TCON &= ~0x30;                      // Stop Timer0; Clear TF0
-   TMOD &= ~0x0f;                      // 16-bit free run mode
-   TMOD |=  0x01;
 
-   CKCON |= 0x04;                      // Timer0 counts SYSCLKs
-
-   while (ms) {
-      TR0 = 0;                         // Stop Timer0
-      TH0 = -(SYSCLK/1000 >> 8);       // Overflow in 1ms
-      TL0 = -(SYSCLK/1000);
-      TF0 = 0;                         // Clear overflow indicator
-      TR0 = 1;                         // Start Timer0
-      while (!TF0);                    // Wait for overflow
-      ms--;                            // Update ms counter
-   }
-
-   TR0 = 0;                            // Stop Timer0
-}
+volatile unsigned char dat;         // Test counter
+volatile unsigned char data_count;  // SMB_DATA_IN and SMB_DATA_OUT counter
+unsigned char i;                    // Dummy variable counters
 
 
 void ACCE_begin()
 {
+   PCA0MD &= ~0x40;                    // WDTE = 0 (watchdog timer enable bit)
+
+   OSCICN |= 0x03;                     // Set internal oscillator to highest
+                                       // setting of 24500000
+
+   // If slave is holding SDA low because of an improper SMBus reset or error
+   while(!SDA)
+   {
+      // Provide clock pulses to allow the slave to advance out
+      // of its current state. This will allow it to release SDA.
+      XBR1 = 0x40;                     // Enable Crossbar
+      SCL = 0;                         // Drive the clock low
+      for(i = 0; i < 255; i++);        // Hold the clock low
+      SCL = 1;                         // Release the clock
+      while(!SCL);                     // Wait for open-drain
+                                       // clock output to rise
+      for(i = 0; i < 10; i++);         // Hold the clock high
+      XBR1 = 0x00;                     // Disable Crossbar
+   }
+
+   SMBus_Init ();                      // Configure and enable SMBus
+
+   EIE1 |= 0x01;                       // Enable the SMBus interrupt
+
+   LED = 0;
+
+   EA = 1;                             // Global interrupt enable
+
+// TEST CODE-------------------------------------------------------------------
+
+   dat = 0;                            // Output data counter
+   NUM_ERRORS = 0;                     // Error counter
     SMBUS_start();
 
     // Power ON
     SMBUS_write_address(ACCE_ADDR, WRITE);
     SMBUS_write_value(POWER_CTRL);
     SMBUS_write_value(0x08); // Measurement Mode
-    //SMBUS_stop(); //?????????????????????????????????????????
 }
 
-void ACCE_read()
+void ACCE_read(
+    unsigned char* ACCE_X0,
+    unsigned char* ACCE_X1,
+    unsigned char* ACCE_Y0,
+    unsigned char* ACCE_Y1,
+    unsigned char* ACCE_Z0,
+    unsigned char* ACCE_Z1
+)
 {
-    unsigned char ACCE_X0;
-    unsigned char ACCE_X1;
-    unsigned char ACCE_Y0;
-    unsigned char ACCE_Y1;
-    unsigned char ACCE_Z0;
-    unsigned char ACCE_Z1;
+   dat = 0;                            // Output data counter
+   NUM_ERRORS = 0;                     // Error counter
+   while (1)
+   {
+      // SMBus Write Sequence
+      for (data_count = 0; data_count < NUM_BYTES_WR; data_count++)
+      {
+         SMB_DATA_OUT[data_count] = dat; // Define next outgoing byte
+         dat++;
+      }
+      TARGET = SLAVE_ADDR;             // Target the Slave for next SMBus
+                                       // transfer
+      SMB_Write();                     // Initiate SMBus write
 
-    SMBUS_start(); //????????????????????????????????????????
+      // SMBus Read Sequence
+      TARGET = SLAVE_ADDR;             // Target the Slave for next SMBus
+                                       // transfer
+      SMB_Read();
 
-    SMBUS_write_address(ACCE_ADDR, READ);
-    SMBUS_write_value(DATAX0); //????????????????????????????
+      // Check transfer data
+      for (data_count = 0; data_count < NUM_BYTES_RD; data_count++)
+      {
+         // Received data match transmit data?
+         if(SMB_DATA_IN[data_count] != SMB_DATA_OUT[data_count])
+         {
+            NUM_ERRORS++;              // Increment error counter if no
+                                       // match
+         }
+      }
 
-    // read char
-    ACCE_X0 = SMB0DAT;
-    ACK = 1;
-    ACCE_X1 = SMB0DAT;
-    ACK = 1;
-    ACCE_Y0 = SMB0DAT;
-    ACK = 1;
-    ACCE_Y1 = SMB0DAT;
-    ACK = 1;
-    ACCE_Z0 = SMB0DAT;
-    ACK = 1;
-    ACCE_Z1 = SMB0DAT;
-    ACK = 0; // OU ACK = 1 ??????????????????????????????????
-
-    SMBUS_stop();
+      // Run to here to view the SMB_DATA_IN and SMB_DATA_OUT variable arrays
+      *ACCE_X0 = SMB_DATA_IN[0];
+      *ACCE_X1 = SMB_DATA_IN[1];
+      *ACCE_Y0 = SMB_DATA_IN[2];
+      *ACCE_Y1 = SMB_DATA_IN[3];
+      *ACCE_Z0 = SMB_DATA_IN[4];
+      *ACCE_Z1 = SMB_DATA_IN[5];
+      
+      delay(1);
 }
 
 
